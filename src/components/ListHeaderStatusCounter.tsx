592f1c1ebb97e44dd5c2a737020d6a8c51366e64
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-05 23:10:21 -0300

    Number => number

--- src/components/ListHeaderStatusCounter.tsx
+++ src/components/ListHeaderStatusCounter.tsx
@@ -1,11 +1,11 @@
 interface ListCounterProps {
-  count: Number | String;
+  count: number | string;
 }
 
 export function ListCounter({ count }: ListCounterProps) {
   return (
     <span className="px-2 bg-gray-300-figma rounded-full text-xs text-gray-200-figma">
-      {count.toString()}
+      {count}
     </span>
   );
 }
